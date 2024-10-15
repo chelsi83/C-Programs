08d3e4f619b42ba0a5abe76f86f17551414f8bea
CHELSI SINGH <chelsiji345@gmail.com>
2024-10-15 20:12:03 +0530

    Add files via upload

---
+++ so2n.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	/*do number add krne ke liye number declare krayenge 
+	or woh no. jismein rkhenge woh declare krayenge*/ 
+	int num1, num2, sum;
+	printf("enter a first number");
+	scanf("%d",&num1);
+	printf("enter a second number");
+	scanf("%d",&num2);
+	sum = num1+num2;
+	printf("sum of two numbers is: %d", sum);
+}
