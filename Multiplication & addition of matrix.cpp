f63ae843327809fe83538f5fd9cba56b93f15bd1
CHELSI SINGH <chelsiji345@gmail.com>
2024-10-22 23:48:15 +0530

    Update Multiplication & addition of matrix.cpp

--- Multiplication & addition of matrix.cpp
+++ Multiplication & addition of matrix.cpp
@@ -3,17 +3,19 @@ int main()
 {
     int rows,cols;
 
-    // Ask for the dimensions of the matrices
-    printf("Enter the number of rows and columns of the matrices: ");
+    // take input from the user for the dimensions of the matrices
+    printf("Enter the number of rows and columns of the matrices");
     scanf("%d %d", &rows, &cols);
 
     // Declare two matrices and a result matrix
     int matrix1[rows][cols], matrix2[rows][cols], result[rows][cols];
 
     // Input elements for the first matrix
-    printf("Enter the elements of the first matrix:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    printf("Enter the elements of the first matrix\n");
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++)
+            {
             printf("Element at [%d][%d]: ", i, j);
             scanf("%d", &matrix1[i][j]);
         }
@@ -21,17 +23,21 @@ int main()
 
     // Input elements for the second matrix
     printf("Enter the elements of the second matrix:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    for (int i = 0; i < rows; i++)
+        {
+        for (int j = 0; j < cols; j++)
+            {
             printf("Element at [%d][%d]: ", i, j);
             scanf("%d", &matrix2[i][j]);
         }
     }
 
     // Perform addition of the matrices
-    printf("\nMatrix Addition Result:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    printf("\nMatrix Addition Result\n");
+    for (int i = 0; i < rows; i++) 
+    {
+        for (int j = 0; j < cols; j++) 
+        {
             result[i][j] = matrix1[i][j] + matrix2[i][j];
             printf("%d\t", result[i][j]);
         }
@@ -39,9 +45,11 @@ int main()
     }
 
     // Perform subtraction of the matrices
-    printf("\nMatrix Subtraction Result:\n");
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
+    printf("\nMatrix Subtraction Result\n");
+    for (int i = 0; i < rows; i++)
+        {
+        for (int j = 0; j < cols; j++) 
+        {
             result[i][j] = matrix1[i][j] - matrix2[i][j];
             printf("%d\t", result[i][j]);
         }
