4a7478c32877f25a777e834a6d3bb6c011d909de
CHELSI SINGH <chelsiji345@gmail.com>
2024-11-28 23:07:14 +0530

    Add files via upload

---
+++ Insert node at beg. of singly linked list.cpp
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    if (!newNode) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the beginning of the list
+struct Node* insertAtBeginning(struct Node* head, int data) {
+    struct Node* newNode = createNode(data);
+    newNode->next = head;
+    head = newNode;
+    return head;
+}
+
+// Function to display the list
+void displayList(struct Node* head) {
+    struct Node* temp = head;
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+}
+
+// Function to free the allocated memory
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head) {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main() {
+    struct Node* head = NULL;
+    int choice, data;
+
+    printf("Singly Linked List - Insert at Beginning\n");
+    while (1) {
+        printf("\n1. Insert a node at the beginning\n");
+        printf("2. Display the list\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter the data to insert: ");
+                scanf("%d", &data);
+                head = insertAtBeginning(head, data);
+                printf("Node inserted successfully.\n");
+                break;
+
+            case 2:
+                printf("The current list is: ");
+                displayList(head);
+                break;
+
+            case 3:
+                freeList(head);
+                printf("Exiting...\n");
+                return 0;
+
+            default:
+                printf("Invalid choice! Please try again.\n");
+        }
+    }
+}
+
